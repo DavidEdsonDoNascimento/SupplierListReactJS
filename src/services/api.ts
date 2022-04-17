5882165912ffcb150987892d7ce80c542c8e143b
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2022-04-17 11:56:23 -0300

    FEAT: #master - Added validations with react-toastify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const port = window.location.href.split('localhost:')[1].substring(0, 4);
 export const api = axios.create({
-    baseURL: 'http://localhost:3003/api',
+    baseURL: `http://localhost:${port}/api`,
 })
